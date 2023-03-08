defe4ae8ebf48acf30a5ea4cf6138ab492932504
Monsoon <107798664+SincerelyAdel@users.noreply.github.com>
2023-03-08 03:19:37 +0300

    General Update
    Updated my small calculator to its second version, this update included overall HTML restructure (lots of div-wrapping and renaming), centering a lot of items in different ways in CSS, and renaming the result div in JS. Overall, a huge update!

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ function advanced_add() {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
 
-    document.getElementById("demo").innerHTML = parseInt(x) + parseInt(y);
+    document.getElementById("Result").innerHTML = parseInt(x) + parseInt(y);
 }
 
 function advanced_subtract() {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
 
-    document.getElementById("demo").innerHTML = parseInt(x) - parseInt(y);
+    document.getElementById("Result").innerHTML = parseInt(x) - parseInt(y);
 }
 
 function advanced_division () {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
 
-    document.getElementById("demo").innerHTML = parseInt(x) / parseInt(y);
+    document.getElementById("Result").innerHTML = parseInt(x) / parseInt(y);
 }
 
 function advanced_multiplication () {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
 
-    document.getElementById("demo").innerHTML = parseInt(x) * parseInt(y);
+    document.getElementById("Result").innerHTML = parseInt(x) * parseInt(y);
 }
