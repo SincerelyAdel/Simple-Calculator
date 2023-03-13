6aa787ecfe0d8b98f79609c560f9ad58f3039dbd
Monsoon <107798664+SincerelyAdel@users.noreply.github.com>
2023-03-14 01:47:01 +0300

    General Update
    This decently-made calculator got its next big update!
    SCalulator version 3 (or V0.2) is a huge batch containing many changes:
    for html I changed the title, modified some class names and id's that I wasn't serious about (till now) and added some for a few elements, changed the flow of my HTML structure and added advanced operations (Powers and roots) to the calculator! (also added more div wrappers than I would've liked).
    
    for CSS I changed the BG for many elements and grouped the common styling for many elements as well, cleaned up some rubble and added styling for the new features!
    
    for JS, I added the new operations and updated all operations to detect wrong or empty input! (was fun).
    
    Overall, update 3 was an amazing batch! I really enjoyed making it!

--- app.js
+++ app.js
@@ -1,27 +1,107 @@
 function advanced_add() {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
-
-    document.getElementById("Result").innerHTML = parseInt(x) + parseInt(y);
+    console.log(typeof x, "hello");
+    if (x === "" || y === "") {
+        document.getElementById("Result").innerHTML = "You need to enter something!";
+        // console.log(x);
+    }
+    else if (Number.isInteger(parseInt(x)) && Number.isInteger(parseInt(y))) {
+        document.getElementById("Result").innerHTML = `the result is ${parseInt(x) + parseInt(y)}`;
+        // console.log(x, typeof parseInt(x));
+    }
+    else {
+        document.getElementById("Result").innerHTML = "Wrong Input! you can only enter number please!";
+        // console.log(parseInt(x, typeof x));
+    }
 }
 
 function advanced_subtract() {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
 
-    document.getElementById("Result").innerHTML = parseInt(x) - parseInt(y);
+    if (x === "" || y === "") {
+        document.getElementById("Result").innerHTML = "You need to enter something!";
+        // console.log(x);
+    }
+    else if (Number.isInteger(parseInt(x)) && Number.isInteger(parseInt(y))) {
+        document.getElementById("Result").innerHTML = `the result is ${parseInt(x) - parseInt(y)}`;
+        // console.log(x, typeof parseInt(x));
+    }
+    else {
+        document.getElementById("Result").innerHTML = "Wrong Input! you can only enter number please!";
+        // console.log(parseInt(x, typeof x));
+    }
 }
 
-function advanced_division () {
+function advanced_division() {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
 
-    document.getElementById("Result").innerHTML = parseInt(x) / parseInt(y);
+    if (x === "" || y === "") {
+        document.getElementById("Result").innerHTML = "You need to enter something!";
+        // console.log(x);
+    }
+    else if (Number.isInteger(parseInt(x)) && Number.isInteger(parseInt(y))) {
+        document.getElementById("Result").innerHTML = `the result is ${parseInt(x) / parseInt(y)}`;
+        // console.log(x, typeof parseInt(x));
+    }
+    else {
+        document.getElementById("Result").innerHTML = "Wrong Input! you can only enter number please!";
+        // console.log(parseInt(x, typeof x));
+    }
 }
 
-function advanced_multiplication () {
+function advanced_multiplication() {
     let x = document.getElementById("fnum").value;
     let y = document.getElementById("snum").value;
 
-    document.getElementById("Result").innerHTML = parseInt(x) * parseInt(y);
+    if (x === "" || y === "") {
+        document.getElementById("Result").innerHTML = "You need to enter something!";
+        // console.log(x);
+    }
+    else if (Number.isInteger(parseInt(x)) && Number.isInteger(parseInt(y))) {
+        document.getElementById("Result").innerHTML = `the result is ${parseInt(x) * parseInt(y)}`;
+        // console.log(x, typeof parseInt(x));
+    }
+    else {
+        document.getElementById("Result").innerHTML = "Wrong Input! you can only enter number please!";
+        // console.log(parseInt(x, typeof x));
+    }
+}
+
+function advanced_power() {
+    let x = document.getElementById("fnum").value;
+    let y = document.getElementById("snum").value;
+
+    if (x === "" || y === "") {
+        document.getElementById("Result").innerHTML = "You need to enter something!";
+        // console.log(x);
+    }
+    else if (Number.isInteger(parseInt(x)) && Number.isInteger(parseInt(y))) {
+        document.getElementById("Result").innerHTML = `the result is ${Math.pow(parseInt(x), parseInt(y))}`;
+        console.log(x, typeof parseInt(x));
+    }
+    else {
+        document.getElementById("Result").innerHTML = "Wrong Input! you can only enter number please!";
+        // console.log(parseInt(x, typeof x));
+    }
+}
+
+function advanced_root() {
+    let x = document.getElementById("fnum").value;
+    let y = document.getElementById("snum").value;
+
+    if (x === "" || y === "") {
+        document.getElementById("Result").innerHTML = "You need to enter something!";
+        // console.log(x);
+    }
+    else if (Number.isInteger(parseInt(x)) && Number.isInteger(parseInt(y))) {
+        document.getElementById("Result").innerHTML = `the result is ${Math.pow(parseInt(x), 1 / parseInt(y))}`;
+        console.log(x, typeof parseInt(x));
+    }
+    else {
+        document.getElementById("Result").innerHTML = "Wrong Input! you can only enter number please!";
+        // console.log(parseInt(x, typeof x));
+    }
 }
